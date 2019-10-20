6153f157cd5367aa7d1e469dc1fbbc7c31471e65
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-10-20 22:37:53 +1100

    x

--- Zombie Survival/Assets/Scripts/Enemies/EnemySpawner.cs
+++ Zombie Survival/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    //Move to GameManager:
     public bool enemiesDead = true;
+    public int round = 0;
+    public int enemiesToSpawn = 8;
+
+    //General:
     public bool enemiesSpawning = false;
     public bool finishedSpawning = true;
 
-    public int enemiesToSpawn = 8;
-    public int round = 0;
-
+    //References:
     public GameObject spawnParent;
     public Transform[] spawnPoints;
     public GameObject enemyParent; 
@@ -28,7 +31,7 @@ public class EnemySpawner : MonoBehaviour
 
     public void Update()
     {
-        if (enemiesDead == true && finishedSpawning == true)
+        if (enemiesDead == true && finishedSpawning == true) //Checks whether or not all enemies are dead and that all enemies have ceased spawning to allow for a new round to start
         {
             round++;
             enemiesSpawning = true;
@@ -36,31 +39,31 @@ public class EnemySpawner : MonoBehaviour
             finishedSpawning = false;
         }
 
-        if (enemiesSpawning == true)
+        if (enemiesSpawning == true) //Checks whether or not enemies are needed to be spawned
         {
             int sT = 0;
-            for (int i = 1; i < spawnPoints.Length; i++)
+            for (int i = 1; i < spawnPoints.Length; i++) //Used to cycle through spawnpoints
             { 
-                StartCoroutine(Spawn(i, sT));
-                enemiesToSpawn--;
-                sT += 3;
-                if (enemiesToSpawn > 0 && i == spawnPoints.Length - 1)
+                StartCoroutine(Spawn(i, sT)); //Spawns an enemy
+                enemiesToSpawn--; //Decreases the enemiesToSpawn counter
+                sT += 3; //Adds to the existing spawn rate timer
+                if (enemiesToSpawn > 0 && i == spawnPoints.Length - 1) //Checks whether or not enemies are still needed to be spawned and if the spawnpoint's array has reached the final spawnpoint to allow for the spawning to loop
                 {
-                    i = 0;
+                    i = 0; //resets the loop
                 }
-                else if (enemiesToSpawn == 0)
+                else if (enemiesToSpawn == 0) //If the enemiesToSpawn count becomes 0, ends enemy spawning
                 {
-                    i = spawnPoints.Length;
-                    enemiesSpawning = false;
-                    finishedSpawning = true;
+                    i = spawnPoints.Length; //ends the loop
+                    enemiesSpawning = false; //Ceases the ability to spawn more enemies
+                    finishedSpawning = true; 
                 }
             }
         }
     }
 
-    IEnumerator Spawn(int spawnIndex, int spawnTime)
+    IEnumerator Spawn(int spawnIndex, int spawnTime) //Used to spawn enemies
     {
-        yield return new WaitForSeconds(spawnTime);
-        GameObject enemy = Instantiate(enemyParent, spawnPoints[spawnIndex].position, Quaternion.identity, spawnPoints[spawnIndex]);
+        yield return new WaitForSeconds(spawnTime); //Determines the amount of time between each set spawn
+        GameObject enemy = Instantiate(enemyParent, spawnPoints[spawnIndex].position, Quaternion.identity, spawnPoints[spawnIndex]); //Instantiates a new enemy based on the spawnpoint arry's index
     }
 }
