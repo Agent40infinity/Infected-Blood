46d2ee5bb66f46018ca15fc2cce584af956e4fc1
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-10-22 09:14:42 +1100

    x

--- Zombie Survival/Assets/Scripts/Player/Weapon.cs
+++ Zombie Survival/Assets/Scripts/Player/Weapon.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //General:
+    private string name;
+    private int id;
+    private float reloadTime;
+    private float fireRate;
+    private float spread;
+    private FireType function;
 
+    //References:
+    private GameObject gunObject;
+
+    //Public Properties:
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int ID
     {
-
+        get { return id; }
+        set { id = value; }
     }
+
+    public float ReloadTime
+    {
+        get { return reloadTime; }
+        set { reloadTime = value; }
+    }
+
+    public float FireRate
+    {
+        get { return fireRate; }
+        set { fireRate = value; }
+    }
+
+    public float Spread
+    {
+        get { return spread; }
+        set { spread = value; }
+    }
+
+    public FireType Function
+    {
+        get { return function; }
+        set { function = value; }
+    }
+
+    public GameObject Gun
+    {
+        get { return gunObject; }
+        set { gunObject = value; }
+    }
+}
+
+public enum FireType
+{
+    Hitscan,
+    Projectile,
+    Entity,
+    Shotgun
 }
---
+++ Zombie Survival/Assets/Scripts/Player/WeaponType.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WeaponType
+{
+    public static Weapon AddWeapon(string Name)
+    {
+        int id = 0;
+        float reloadTime = 0;
+        float fireRate = 0;
+        float spread = 0;
+        FireType function = FireType.Hitscan;
+        GameObject gunObject = Resources.Load("Prefabs/Gun") as GameObject;
+
+        switch (Name)
+        {
+            case "Pistol":
+                id = 0;
+                reloadTime = 2f;
+                fireRate = 0.4f;
+                spread = 1;
+                function = FireType.Hitscan;
+                break;
+            case "Rifle":
+                id = 1;
+                reloadTime = 5f;
+                fireRate = 0.3f;
+                spread = 3;
+                function = FireType.Hitscan;
+                break;
+            case "SMG":
+                id = 2;
+                reloadTime = 3.5f;
+                fireRate = 0.1f;
+                spread = 5;
+                function = FireType.Projectile;
+                break;
+            case "Shotgun":
+                id = 3;
+                reloadTime = 2f;
+                fireRate = 1f;
+                spread = 1;
+                function = FireType.Hitscan;
+                break;
+            case "Flamethrower":
+                id = 4;
+                reloadTime = 6f;
+                fireRate = 0.2f;
+                spread = 1;
+                function = FireType.Entity;
+                break;
+            case "BFG":
+                id = 5;
+                reloadTime = 4f;
+                fireRate = 3f;
+                spread = 1;
+                function = FireType.Projectile;
+                break;
+        }
+
+        Weapon temp = new Weapon
+        {
+            Name = Name,
+            ID = id,
+            ReloadTime = reloadTime,
+            FireRate = fireRate,
+            Spread = spread,
+            Function = function,
+            Gun = gunObject
+        };
+        return temp;
+    }
+}
