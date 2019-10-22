9b6c42882b8be148ab823110ce43d0a3a3a85617
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-10-22 11:59:07 +1100

    x

--- Zombie Survival/Assets/Scripts/Enemies/Enemy.cs
+++ Zombie Survival/Assets/Scripts/Enemies/Enemy.cs
@@ -16,11 +16,24 @@ public class Enemy : MonoBehaviour
 
     public void Update()
     {
+        Collider[] inRange = Physics.OverlapSphere(gameObject.transform.position, 10f);
+        for (int i = 0; i < inRange.Length; i++)
+        {
+            if (inRange[i].tag == "Player" && (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) > 10 || Mathf.Abs(player.transform.position.z - gameObject.transform.position.x) > 10))
+            {
+                player = inRange[i].gameObject;
+            }
+        }
         nav.SetDestination(player.transform.position);
     }
 
     private void OnDestroy()
     {
         GameManager.enemiesAlive--;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
     }
 }
