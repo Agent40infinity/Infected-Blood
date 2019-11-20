ebba435a89ff2f068bcb8b93035a6d14080f528e
Agent40infinity <48039243+Agent40infinity@users.noreply.github.com>
2019-11-20 12:28:28 +1100

    x

---
+++ Assets/Scripts/Utility/HUD.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public GameManager gameManager;
+    public GameObject scoreboard;
+
+    public void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        scoreboard = GameObject.Find("Scoreboard");
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            scoreboard.SetActive(true);
+            displayStats();
+        }
+        else
+        {
+            scoreboard.SetActive(false);
+        }
+    }
+
+    public void displayStats()
+    {
+        
+    }
+
+    public void BeginGame()
+    {
+        gameManager.StartGame();
+    }
+}
