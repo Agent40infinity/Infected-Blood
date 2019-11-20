dbb359dba2768a18fa87626dac5949353782b282
Jack <47583484+JackADV@users.noreply.github.com>
2019-11-20 12:05:13 +1100

    Update

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public int curAmmo;
+    public int maxAmmo;
+    public bool isFiring;
+    public Text ammoDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoDisplay.text = curAmmo +"/"+ maxAmmo;
+       // ammoDisplay.text = maxAmmo.ToString();
+        if(Input.GetMouseButtonDown(0) && !isFiring && curAmmo >0)
+        {
+            isFiring = true;
+            curAmmo--;
+            isFiring = false;
+        }
+    }
+}
